22a7bdfd6eb8676ae984d1b4e48c4666c9c69852
piusrauch <pirauch@tsn.at>
2024-10-03 13:46:36 +0200

    Filme und Ebooks als Klasse gemacht und diese von Medium extended

---
+++ src/EBooks.java
@@ -0,0 +1,15 @@
+public class EBooks extends Medium{
+    private String titel;
+    private String autor;
+    private int isbn;
+    private int seitenanzahl;
+    private String auflage;
+    private String kommentar;
+    private int erscheinungsdatum;
+    private String genre;
+
+
+
+
+
+}
---
+++ src/Filme.java
@@ -0,0 +1,13 @@
+public class Filme extends Medium{
+
+    private String titel;
+    private String regisseur;
+    private double spielzeit;
+    private String aufloesung;
+    private String kommentar;
+    private int erscheinungsdatum;
+    private String genre;
+
+
+
+}
